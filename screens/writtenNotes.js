b6cbd9a3e5a5485edc68bc997de75fa84aa6ae99
Manasa <Manasa9955j@gmail.com>
2021-05-25 21:41:57 +0530

    v4

--- screens/writtenNotes.js
+++ screens/writtenNotes.js
@@ -1,13 +1,72 @@
 import * as React from "react"
 import {View,TouchableOpacity,Text, KeyboardAvoidingView, TextInput,StyleSheet} from "react-native"
 import {Header} from "react-native-elements"
+import db from "../config"
+import firebase from "firebase"
+import {ListItem} from "react-native-elements"
+import { FlatList } from "react-native"
+
 
 export default class WrittenNotes extends React.Component{
-  
+  constructor(){
+      super();
+      this.state={
+          userId:firebase.auth().currentUser.email,
+          writtenNotesList:[]
+      }
+      this.requestRef=null;
+  }
+  getWrittenNotesList=()=>{
+      this.requestRef=db.collection("writtenNotes").where(userId,"==",this.state.userId)
+      .onSnapshot((snapShot)=>{
+          var writtenNotesList= snapShot.docs.map((doc)=>doc.data());
+          this.setState({
+              writtenNotesList:writtenNotesList
+          })
+      })
+    }
+    componentDidMount(){
+        this.getWrittenNotesList();
+    }
+    componentWillUnmount(){
+        this.requestRef();
+    }
+    keyExtractor=(item,index)=>{
+        index.toString();
+    };
+    renderItem=({item,i})=>{
+        return (
+            <ListItem
+            key={i}
+            title={item.subject}
+            subtitle={item.topic}
+            titleStyle={{color:"blue",fontWeight:"bold"}}
+            rightElement={
+                <TouchableOpacity 
+                onPress={()=>{
+                    this.props.navigation.navigate("addWrittenNotes",{
+                    details:item
+                })
+                }}
+                ><Text>View</Text></TouchableOpacity>
+            }
+            />
+        )
+    }
     render(){
         return(
             <KeyboardAvoidingView>
-               <Text>written</Text>
+               {this.state.writtenNotesList.length===0?(
+                   <View>
+                       <Text>Loading............</Text>
+                   </View>
+               ):(
+                   <FlatList 
+                   keyExtractor={this.keyExtractor}
+                   data={this.state.writtenNotesList}
+                   renderItem={this.renderItem}
+                   />
+               )}
                
             </KeyboardAvoidingView>
         )
